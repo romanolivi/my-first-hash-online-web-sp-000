a195105c65d03be4239394a2e84c901ade9967a4
Romano Livi <romano_livi@outlook.com>
2020-12-14 18:32:53 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -21,8 +21,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  return shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -31,14 +30,9 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
+  shipping_manifest['muskets'] = 2
+  shipping_manifest["gun powder"] = 4
+  
 
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
+shipping_manifest
 end
